0279c9c3a67abc34ed5413272c634a889c54aff2
SammuelBlebo <samuel.blebo@yahoo.com>
2023-04-17 09:23:04 +0000

    ...

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,34 @@
+import "./Header.css";
+import logo from "../assets/logo.svg";
+import { VscGithub, VscTwitter } from "react-icons/vsc";
+import { BsLinkedin } from "react-icons/bs";
+
 export default function Header() {
   return (
-    <div>
-      <h1>Hello, World</h1>
+    <div className="header">
+      <div className="logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="menu">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+      <div className="socials">
+        <div className="social icon">
+          <VscGithub />
+        </div>
+        <div className="social icon">
+          <VscTwitter />
+        </div>
+        <div className="social icon">
+          <BsLinkedin />
+        </div>
+      </div>
     </div>
   );
 }
