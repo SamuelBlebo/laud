ea23b57ad52008b43ad9ad4b55933dee2db32645
Samuel Blebo <samuel.blebo@yahoo.com>
2023-04-19 10:33:15 +0000

    Menu Items and Icons Added

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -1,3 +1,5 @@
+import { VscGithub, VscTwitter } from "react-icons/vsc";
+import { BsLinkedin } from "react-icons/bs";
 import { HiMoon, HiSun } from "react-icons/hi";
 import { MdOutlineCancel } from "react-icons/md";
 import "./Menu.css";
@@ -21,6 +23,36 @@ export default function Menu({ isDarkMode, toggleDarkMode }) {
         </div>
         <div>
           <MdOutlineCancel onClick={hideMenu} />
+        </div>
+      </div>
+      <div className="menu-items">
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="portfolio">Portfolio</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-socials">
+        <div>
+          <VscGithub />
+        </div>
+        <div>
+          <VscTwitter />
+        </div>
+        <div>
+          <BsLinkedin />
         </div>
       </div>
     </div>
