04447b7246439e119d81c3a02696bf6ec2aeefe1
SammuelBlebo <samuel.blebo@yahoo.com>
2023-05-02 06:31:15 +0000

    .

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,8 +1,20 @@
 import "./Hero.css";
-export default function Hero(params) {
+export default function Hero() {
   return (
     <div className="hero">
-      <h1>Hello, Hero</h1>
+      <div className="hero-container">
+        <div className="hero-section-text">
+          <div className="hero-text-title">
+            <h1>App & Software Developer</h1>
+          </div>
+          <div className="hero-sub-text">
+            <p>Laud Miller</p>
+          </div>
+          <div className="contact-button ">
+            <button className="button">Contact Me</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
