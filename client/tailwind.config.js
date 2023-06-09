188f8dbb98b69ee3281781037344e3372560abf3
SammuelBlebo <samuel.blebo@yahoo.com>
2023-06-09 06:53:32 +0000

    .

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Jost", "sans-serif"],
+      body: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
