e5680a6c747f6e447633a64245f3ab1024892864
Samuel Blebo <samuel.blebo@yahoo.com>
2023-06-17 17:39:59 +0000

    .

--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -3,14 +3,14 @@ export default function Hero({ isActive }) {
     <div className={`${isActive ? "bg-black text-white " : ""}`}>
       <div className="border-x border-b mx-14 flex h-[100vh]">
         <div className="info w-6/10 pl-14 lg:pl-24 xl:pl-24 flex flex-col justify-center items-start">
-          <h2 className="text-7xl xl:text-8xl font-extrabold">
+          <h2 className="text-lg 2xl:text-8xl font-extrabold">
             App & Software Developer
           </h2>
-          <p className="my-7 text-md xl:text-xl text-zinc-500">LAUD MULLER</p>
+          <p className="my-7  text-md 2xl:text-xl text-zinc-500">LAUD MULLER</p>
           <button
             className={`${
               isActive ? "bg-zinc-800" : ""
-            } bg-zinc-300 hover:bg-orange-600 px-9 xl:px-14 py-2.5 xl:py-4 `}
+            } bg-zinc-300 hover:bg-orange-600 px-9 2xl:px-14 py-2.5 2xl:py-4 `}
           >
             Contact
           </button>
