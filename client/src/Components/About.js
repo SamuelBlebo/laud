72234f0207c74f2da422b9c1903dd9217955a414
SammuelBlebo <samuel.blebo@yahoo.com>
2023-06-25 18:12:12 +0000

    .

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -8,7 +8,7 @@ export default function About({ isActive }) {
             alt="Placeholder"
           />
         </div>
-        <div className="px-8 pt-32 lg:pt-0 md:pt-20 info flex flex-col justify-center items-center lg:w-6/10 lg:pl-24 xl:pl-24 lg:justify-center lg:items-start">
+        <div className="px-8 pt-32 lg:pt-0 md:pt-20 info flex flex-col lg:w-6/10 lg:pl-24 xl:pl-24 lg:justify-center lg:items-start">
           <h2 className="text-3xl lg:text-6xl lg:text-start 2xl:text-8xl font-extrabold">
             App & Software Developer
           </h2>
@@ -16,7 +16,7 @@ export default function About({ isActive }) {
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa
             magnam quam tempora unde blanditiis!
           </h5>
-          <p>
+          <p className="mt-8 mb-20 ">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis
             deserunt quis doloremque provident natus, quod quam ad facere nisi
             praesentium incidunt voluptas repudiandae blanditiis ullam quisquam
@@ -28,7 +28,7 @@ export default function About({ isActive }) {
           <button
             className={`${
               isActive ? "bg-zinc-800" : ""
-            } bg-zinc-300 hover:bg-orange-600 px-9 2xl:px-14 py-2.5 2xl:py-4 `}
+            } bg-zinc-300 hover:bg-orange-600 px-9 2xl:px-14 py-2.5 2xl:py-4`}
           >
             Contact
           </button>
