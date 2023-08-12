863ee4589652a2e3bcb4cc2e51c8191f06c568df
SammuelBlebo <samuel.blebo@yahoo.com>
2023-08-12 02:06:50 +0000

    .

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import Logo from "../Assets/LogoLaud.png";
+import MobileMenuIcon from "../Assets/MobileMenuIcon.png";
 
 export default function Header() {
   return (
-    <div className="header w-auto h-[70px] bg-[#000000] text-[#fff]  border-[#E5540A] border-b-2 flex flex-row items-center justify-between ">
+    <div className="header w-auto h-[58px] lg:h-[70px] bg-[#000000] text-[#fff]  border-[#E5540A] border-b-2 flex flex-row items-center justify-between ">
       <div className="logo">
-        <img src={Logo} alt="Logo" className="h-[60px] w-[60px] ml-[25px]" />
+        <img
+          src={Logo}
+          alt="Logo"
+          className=" w-[40px] h-[40px] lg:h-[60px]  lg:w-[60px]  ml-[25px]"
+        />
       </div>
-      <div className="navigation hidden xl:block ">
+      <div className="navigation hidden lg:block ">
         <ul className="flex">
           <li className="mr-[34px]">
             <a href="/" className="text-[#E5540A] hover:text-[#E5540A]">
@@ -36,8 +41,11 @@ export default function Header() {
           </li>
         </ul>
       </div>
-      <div className="email-me mr-[25px] px-5 py-[6px] bg-[#000] border-[#fff] border-2">
+      <div className="email-me hidden lg:block mr-[25px] px-5 py-[6px] bg-[#000] border-[#fff] border-2">
         Email Me
+      </div>
+      <div className="mobile-menu-icon lg:hidden">
+        <img src={MobileMenuIcon} alt="" className="h-[30px] mr-[25px]" />
       </div>
     </div>
   );
