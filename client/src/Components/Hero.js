b27dcec1ed5ba74e74de26ded161f6b6f40c8732
SammuelBlebo <samuel.blebo@yahoo.com>
2023-08-15 04:38:55 +0000

    Mobile view for hero

--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -3,9 +3,9 @@ import HeroImage from "../Assets/HeroImage.png";
 
 export default function Hero() {
   return (
-    <div className="background h-auto lg:h-[700px] xl:h-[800px] w-auto bg-[#000] p-[25px] flex items-center justify-between">
-      <div className="hero-info lg:pl-[80px] xl:pl-[110px] 2xl:pl-[250px] ">
-        <h1 className="text-[#fff] lg:text-[70px] xl:text-[80px] font-bold lg:leading-[72px] xl:leading-[92px] lg:absolute lg:top-[260px] xl:absolute xl:top-[300px]">
+    <div className="background h-auto lg:h-[700px] xl:h-[800px] w-auto bg-[#000] p-[25px] lg:flex items-center justify-between">
+      <div className="hero-info px-[32px] lg:pl-[80px] xl:pl-[110px] 2xl:pl-[250px] ">
+        <h1 className="text-[#fff] text-[34px] lg:text-[70px] xl:text-[80px] font-bold lg:leading-[72px] xl:leading-[92px] lg:absolute lg:top-[260px] xl:absolute xl:top-[300px]">
           App & Software <br /> Developer
         </h1>
         <h5 className="text-[#fff] lg:absolute lg:top-[430px] xl:absolute xl:top-[500px]">
