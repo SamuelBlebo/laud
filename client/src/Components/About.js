ea021a6ae03bd99d48e2a4b603426f95fd78f7fb
SammuelBlebo <samuel.blebo@yahoo.com>
2023-08-20 01:27:19 +0100

    .

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -4,8 +4,8 @@ import AboutMeImage from "../Assets/AboutMe.png";
 
 export default function About() {
   return (
-    <div className="container px-[32px] py-[62px] flex justify-center m-20px">
-      <div className="lg:flex  lg:flex-row-reverse items-center justify-center">
+    <div className="px-[32px] py-[62px] flex  lg:px-[60px] lg:py-[100px] xl:px-[150px] xl:py-[100px] 2xl:px-[300px] 2xl:py-[100px]">
+      <div className="lg:flex  lg:flex-row-reverse lg:items-center lg:justify-center ">
         <div className="about-me-info flex-1">
           <div className="pb-[30px] flex items-center">
             <div className="about-me-text border-2 border-[#E5540A] w-[50px] h-[40px] "></div>
@@ -26,8 +26,12 @@ export default function About() {
             on tekkinud mitmeid versioone, mõnikord kogemata, mõnikord
           </p>
         </div>
-        <div className="flex-1">
-          <img src={AboutMeImage} alt="About Me" />
+        <div className="flex-1 xl:mr-[40px]">
+          <img
+            src={AboutMeImage}
+            alt="About Me"
+            className="lg:h-[500px] lg:w-auto xl:h-[600px]"
+          />
         </div>
       </div>
     </div>
--- client/src/Components/Hero.js
+++ client/src/Components/Hero.js
@@ -4,19 +4,21 @@ import HeroImage from "../Assets/HeroImage.png";
 export default function Hero() {
   return (
     <div className="background h-auto px-[32px] py-[60px] md:py-[120px] md:h-[1200px] lg:h-[700px] xl:h-[800px] w-auto bg-[#000] lg:flex items-center justify-between">
-      <div className="hero-info  md:px-[72px]  lg:pl-[80px] xl:pl-[110px] 2xl:pl-[250px] ">
-        <h1 className="text-[#fff] text-[34px] md:text-[52px] md:leading-[60px] lg:text-[70px] xl:text-[80px] font-bold lg:leading-[72px] xl:leading-[92px] lg:absolute lg:top-[260px] xl:absolute xl:top-[300px]">
+      <div className="hero-info md:px-[72px]  lg:pl-[80px] xl:pl-[110px] 2xl:pl-[250px] ">
+        <h1 className="text-[#fff] text-[40px] leading-[50px] text-center md:text-[52px] md:leading-[60px] lg:text-[70px] xl:text-[80px] lg:text-left font-bold lg:leading-[72px] xl:leading-[92px] lg:absolute lg:top-[260px] xl:absolute xl:top-[300px]">
           App & Software <br /> Developer
         </h1>
-        <div className="laud-text p-0 m-0 flex  lg:justify-center items-center mt-[15px] md:mt-[40px] lg:absolute lg:top-[400px] xl:absolute xl:top-[500px]">
+        <div className="laud-text p-0 m-0 flex justify-center items-center mt-[15px] md:mt-[40px] lg:absolute lg:top-[400px] xl:absolute xl:top-[500px]">
           <h5 className="text-[#fff] ">LAUD MILLER</h5>
-          <hr className="hr border-[1px] border-[#fff] border-x-[50px] mx-4 " />
+          <hr className="hr border-[1px] border-[#fff] border-x-[20px] lg:border-x-[50px] mx-4 " />
         </div>
-        <button className="text-[#fff] bg-[#101010] my-[50px] px-[20px] py-[10px] lg:px-[40px] lg:py-[20px] lg:absolute lg:top-[500px] xl:absolute xl:top-[580px]">
-          Contact Me
-        </button>
+        <div className="flex justify-center lg:block">
+          <button className="text-[#fff]  bg-[#101010] my-[50px] px-[20px] py-[10px] lg:px-[40px] lg:py-[20px] lg:absolute lg:top-[500px] xl:absolute xl:top-[580px]">
+            Contact Me
+          </button>
+        </div>
       </div>
-      <div className="hero-image px-[32px] pt-[30px] md:px-[72px] lg:pr-[80px] xl:pr-[120px] 2xl:pr-[250px]  ">
+      <div className="hero-image px-[18px] pt-[30px] md:px-[72px] lg:pr-[80px] xl:pr-[120px] 2xl:pr-[250px]  ">
         <div className="">
           <img src={HeroImage} alt="Hero" className="h-auto w-[600px]" />
         </div>
