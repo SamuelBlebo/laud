4ef3be83fbbed08a703e7a6d94b54eac280a51c3
SammuelBlebo <samuel.blebo@yahoo.com>
2023-08-21 04:08:51 +0000

    .

--- client/src/Components/Skills.js
+++ client/src/Components/Skills.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Levels from "../Assets/levels.png";
 import CSharp from "../Assets/c-sharp.png";
+import Java from "../Assets/java.png";
+import Python from "../Assets/python.png";
+import Ruby from "../Assets/ruby.png";
 
 export default function Skills() {
   return (
@@ -13,12 +16,12 @@ export default function Skills() {
           On teada-tuntud fakt, et lehe kujunduse vaatamisel juhib selle loetav
           sisu lugeja tähelepanu kõrvale.
         </p>
-        <img src={Levels} alt="Levels" clas />
+        <img src={Levels} alt="Levels" className="lg:w-[350px] lg:h-auto" />
       </div>
-      <div className="flex justify-center mt-[50px] lg:flex-1 lg:mt-0">
+      <div className="flex jst mt-[50px] lg:flex-1 lg:mt-0">
         <div className="flex-1">
-          <div className="lg:flex-1 mt-[20px] lg:mt-0 ">
-            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl lg:h-[200px] lg:w-[200px]">
+          <div className="lg:flex lg:flex-1 mt-[20px] lg:mt-0 lg:justify-center">
+            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl lg:h-[180px] lg:w-[180px]">
               <img
                 src={CSharp}
                 alt=" C-sharp logo"
@@ -26,32 +29,32 @@ export default function Skills() {
               />
             </div>
           </div>
-          <div className="lg:flex-1  mt-[20px] lg:mt-[40px]">
-            <div className=" flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[200px] lg:w-[200px]">
+          <div className="lg:flex lg:flex-1  mt-[20px] lg:mt-[40px] lg:justify-center">
+            <div className=" flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[180px] lg:w-[180px]">
               <img
-                src={CSharp}
-                alt=" C-sharp logo"
-                className="w-auto h-[76px]"
+                src={Java}
+                alt="Ruby Logo"
+                className="w-auto h-[76px] lg:h-[130px]"
               />
             </div>
           </div>
         </div>
         <div className="flex-1 mt-7 ">
-          <div className="lg:flex-1 mt-[20px]">
-            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[200px] lg:w-[200px]">
+          <div className="lg:flex lg:flex-1 mt-[20px] lg:justify-start">
+            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[180px] lg:w-[180px]">
               <img
-                src={CSharp}
-                alt=" C-sharp logo"
-                className="w-auto h-[76px]"
+                src={Python}
+                alt="Python logo"
+                className="w-auto h-[76px] lg:h-[130px]"
               />
             </div>
           </div>
-          <div className="lg:flex-1 mt-[20px] lg:mt-[40px]">
-            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[200px] lg:w-[200px]">
+          <div className="lg:flex lg:flex-1 mt-[20px] lg:mt-[40px] lg:justify-start">
+            <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[180px] lg:w-[180px]">
               <img
-                src={CSharp}
-                alt=" C-sharp logo"
-                className="w-auto h-[76px]"
+                src={Ruby}
+                alt=" Ruby logo"
+                className="w-auto h-[70px] lg:h-[120px]"
               />
             </div>
           </div>
