ea78c4eb75626eaedf654315f94008ae7ba3519c
samuelblebo <samuel.blebo@yahoo.com>
2023-09-24 12:02:42 +0000

    .

--- client/src/Components/Skills.js
+++ client/src/Components/Skills.js
@@ -7,10 +7,10 @@ import Ruby from "../Assets/ruby.png";
 
 export default function Skills() {
   return (
-    <div className="p-[40px] lg:flex  md:px-[110px] lg:px-[60px] xl:px-[150px] 2xl:px-[300px] xl:pb-[140px]">
-      <div className="lg:flex-1 lg:pr-[80px]">
-        <h2 className="text-[32px] font-bold leading-[40px]  pb-[30px]">
-          My development skills includes these technologies.{" "}
+    <div className="px-[70px] lg:flex  md:px-[110px] lg:px-[60px] xl:px-[150px] 2xl:px-[300px] xl:pb-[140px]">
+      <div className="flex flex-col items-center lg:flex-1 lg:pr-[80px]">
+        <h2 className="text-[26px] font-bold leading-[40px]  pb-[30px] lg:text-[32px]">
+          My development skills includes these technologies.
         </h2>
         <p className="font-[16px] leading-[30px] pb-[30px] opacity-80">
           On teada-tuntud fakt, et lehe kujunduse vaatamisel juhib selle loetav
@@ -18,8 +18,8 @@ export default function Skills() {
         </p>
         <img src={Levels} alt="Levels" className="lg:w-[350px] lg:h-auto" />
       </div>
-      <div className="flex jst mt-[50px] lg:flex-1 lg:mt-0">
-        <div className="flex-1">
+      <div className="flex flex-row justify-center my-[50px] mr-[50px] ml-[20px] lg:flex-1 lg:mt-0">
+        <div className="flex-1 pr-[10px]">
           <div className="lg:flex lg:flex-1 mt-[20px] lg:mt-0 lg:justify-center">
             <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl lg:h-[180px] lg:w-[180px]">
               <img
@@ -39,7 +39,7 @@ export default function Skills() {
             </div>
           </div>
         </div>
-        <div className="flex-1 mt-7 ">
+        <div className="flex-1 pt-7 ml-[20px] ">
           <div className="lg:flex lg:flex-1 mt-[20px] lg:justify-start">
             <div className="flex justify-center items-center bg-[#F7F7F7] hover:bg-[#E5540A] rounded-[20px] h-[120px] w-[120px] drop-shadow-2xl  lg:h-[180px] lg:w-[180px]">
               <img
--- client/src/Components/WhatIDo.js
+++ client/src/Components/WhatIDo.js
@@ -5,63 +5,65 @@ import Devops from "../Assets/devops 1.png";
 
 export default function WhatIDo() {
   return (
-    <div className="w-[100vw] h-auto bg-[#000] px-[60px] py-[62px] lg:px-[90px] xl:px-[160px] 2xl:px-[500px] lg:py-[150px]">
-      
-      <div className="pb-[30px] flex items-center">
-        <div className="about-me-text border-2 border-[#E5540A] w-[50px] h-[40px] "></div>
-        <h3 className="text-[#fff] bg-[#000] m-[-40px] p-[2.5px]">What I Do</h3>
-      </div>
+    <div className="w-[100vw] h-auto bg-[#000] px-[18%] py-[90px] lg:px-[90px] xl:px-[160px] 2xl:px-[500px] lg:py-[150px]">
       <div>
-        <h3 className="font-bold text-[#c1c1c1] text-[24px] lg:w-[500px] mb-[30px]">
-          I have the capability to provide these Services.
-        </h3>
-      </div>
-  
+        <div className="pb-[30px] flex items-center">
+          <div className="about-me-text border-2 border-[#E5540A] w-[50px] h-[40px] "></div>
+          <h3 className="text-[#fff] bg-[#000] m-[-40px] p-[2.5px]">
+            What I Do
+          </h3>
+        </div>
+        <div>
+          <h3 className="font-bold text-[#c1c1c1] text-[24px] lg:w-[500px] mb-[30px]">
+            I have the capability to provide these Services.
+          </h3>
+        </div>
 
-      <div className="flex flex-col lg:flex-row ">
-        <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
-          <img src={SmartPhoneIcon} alt="Smart Phone Icon" />
-          <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
-            Mobile <br /> Development
-          </h4>
-          <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
-            quis pariatur rem unde quod praesentium iure, ullam quam nostrum
-            labore tempora dicta error
-          </p>
-          <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
-            Contact me
-          </a>
-        </div>
-        <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
-          <img src={Joystick} alt="Joystick Icon" />
-          <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
-            Game Development
-          </h4>
-          <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
-            quis pariatur rem unde quod praesentium iure, ullam quam nostrum
-            labore tempora dicta error, laborum assumenda quas doloribus.
-            Delectus, quas asperiores.
-          </p>
-          <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
-            Contact me
-          </a>
-        </div>
-        <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
-          <img src={Devops} alt="Devops Icon" />
-          <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
-            Software Development
-          </h4>
-          <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
-            quis pariatur rem unde quod praesentium iure, ullam quam nostrum
-            labore tempora dicta error, laborum assumenda quas doloribus.
-            Delectus, quas asperiores.
-          </p>
-          <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
-            Contact me
-          </a>
+        <div className="flex flex-col lg:flex-row ">
+          <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
+            <img src={SmartPhoneIcon} alt="Smart Phone Icon" />
+            <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
+              Mobile <br /> Development
+            </h4>
+            <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+              quis pariatur rem unde quod praesentium iure, ullam quam nostrum
+              labore tempora dicta error
+            </p>
+            <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
+              Contact me
+            </a>
+          </div>
+          <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
+            <img src={Joystick} alt="Joystick Icon" />
+            <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
+              Game Development
+            </h4>
+            <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+              quis pariatur rem unde quod praesentium iure, ullam quam nostrum
+              labore tempora dicta error, laborum assumenda quas doloribus.
+              Delectus, quas asperiores.
+            </p>
+            <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
+              Contact me
+            </a>
+          </div>
+          <div className="w-[300px] h-[400px] bg-[#1e1e1e] my-6 lg:mr-4 p-[25px]">
+            <img src={Devops} alt="Devops Icon" />
+            <h4 className="text-[20px] font-bold text-[#e5540a] mt-[20px]">
+              Software Development
+            </h4>
+            <p className="text-[#dcdcdc] text-[16.5px] mt-[18px] mb-[15px] leading-[26px] ">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+              quis pariatur rem unde quod praesentium iure, ullam quam nostrum
+              labore tempora dicta error, laborum assumenda quas doloribus.
+              Delectus, quas asperiores.
+            </p>
+            <a href="/contact" className="text-[#e5540a] underline mt-[20px]">
+              Contact me
+            </a>
+          </div>
         </div>
       </div>
     </div>
