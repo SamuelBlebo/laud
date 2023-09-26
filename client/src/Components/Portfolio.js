31910fdc657f8fcc376e8f2ca42bbdb3c823c196
samuelblebo <samuel.blebo@yahoo.com>
2023-09-26 03:06:29 +0000

    .

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -1,6 +1,10 @@
 import React from "react";
 import GomSoftware from "../Assets/GomSoftware.png";
 import JkayApp from "../Assets/JKay Mobile App.png";
+import AdidasRetro from "../Assets/AdidasRetro.png";
+import Piyee from "../Assets/Piyee.png";
+import DPLeeroy from "../Assets/DeepLeeroy.png";
+import Task from "../Assets/Task.io.png";
 import { BsArrowRight } from "react-icons/bs";
 
 export default function Portfolio() {
@@ -19,33 +23,33 @@ export default function Portfolio() {
           </div>
           <div className="apps mt-[40px] lg:flex  lg:mt-[0px]">
             <div className="mr-[40px]">
-              <div className="w-[330px] h-[330px] bg-[#d1d1d1] mt-[20px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
-                <img src={GomSoftware} alt="" />
+              <div className="w-[330px] h-[330px] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[20px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+                <img src={AdidasRetro} alt="" />
                 <div className="flex flex-row items-center align-middle mt-[20px]">
-                  <h3 className="mr-[140px] text-[18px]">Gom Software</h3>
+                  <h3 className="mr-[140px] text-[18px]">Adidas Retro</h3>
                   <BsArrowRight />
                 </div>
               </div>
-              <div className="w-[330px] h-[330px] bg-[#d1d1d1] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
-                <img src={GomSoftware} alt="" />
+              <div className="w-[330px] h-[330px] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+                <img src={Piyee} alt="" />
                 <div className="flex flex-row items-center align-middle mt-[20px]">
-                  <h3 className="mr-[140px] text-[18px]">Gom Software</h3>
+                  <h3 className="mr-[190px] text-[18px]">Piyee</h3>
                   <BsArrowRight />
                 </div>
               </div>
             </div>
             <div>
-              <div className="w-[330px] h-[330px] bg-[#d1d1d1] mt-[30px] lg:mt-[20px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
-                <img src={GomSoftware} alt="" />
+              <div className="w-[330px] h-[330px] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[30px] lg:mt-[20px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+                <img src={DPLeeroy} alt="" />
                 <div className="flex flex-row items-center align-middle mt-[20px]">
-                  <h3 className="mr-[140px] text-[18px]">Gom Software</h3>
+                  <h3 className="mr-[140px] text-[18px]">DP Leeroy</h3>
                   <BsArrowRight />
                 </div>
               </div>
-              <div className="w-[330px] h-[330px] bg-[#d1d1d1] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
-                <img src={GomSoftware} alt="" />
+              <div className="w-[330px] h-[330px] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+                <img src={Task} alt="" />
                 <div className="flex flex-row items-center align-middle mt-[20px]">
-                  <h3 className="mr-[140px] text-[18px]">Gom Software</h3>
+                  <h3 className="mr-[190px] text-[18px]">Task.io</h3>
                   <BsArrowRight />
                 </div>
               </div>
@@ -53,17 +57,17 @@ export default function Portfolio() {
           </div>
         </div>
         <div className="div-without-tex mr-[30px]">
-          <div className="w-[330px] h-[330px] bg-[#d1d1d1] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+          <div className="w-[330px] h-[330px] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[30px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
             <img src={GomSoftware} alt="" />
             <div className="flex flex-row items-center align-middle mt-[20px]">
               <h3 className="mr-[140px] text-[18px]">Gom Software</h3>
               <BsArrowRight />
             </div>
           </div>
-          <div className="w-[330px] h-[auto] bg-[#d1d1d1] mt-[42px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
+          <div className="w-[330px] h-[auto] bg-[#d1d1d1] hover:bg-[#e5540a] mt-[42px] flex flex-col items-center justify-center rounded-[24px] px-[18px] py-[18px]">
             <img src={JkayApp} alt="" />
             <div className="flex flex-row items-center align-middle mt-[20px]">
-              <h3 className="mr-[140px] text-[18px]">JkayApp</h3>
+              <h3 className="mr-[190px] text-[18px]">JkayApp</h3>
               <BsArrowRight />
             </div>
           </div>
