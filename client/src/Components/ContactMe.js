0913301fe82bdb840bc87ec1b3c530a1377067cd
samuelblebo <samuel.blebo@yahoo.com>
2023-10-03 19:33:20 +0000

    .

--- client/src/Components/ContactMe.js
+++ client/src/Components/ContactMe.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsTelephoneFill } from "react-icons/bs";
 
 export default function ContactMe() {
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle the form submission logic here
+    console.log("Form data submitted:", state);
+  };
+
   return (
-    <div className="bg-[#000] px-[16%] py-[50px] ">
+    <div className="bg-[#000] px-[16%] py-[50px]">
       <div className="">
         <div className="pb-[30px] flex items-center">
           <div className="about-me-text border-2 border-[#E5540A] w-[50px] h-[40px] "></div>
@@ -11,22 +28,22 @@ export default function ContactMe() {
             Contact Me
           </h3>
         </div>
-        <div className="bg-[#1E1E1E] w-[60vw] rounded-[20px] flex justify-between items-center px-[25px] py-[10px]">
+        <div className="bg-[#1E1E1E] w-[70vw] rounded-[15px] flex justify-between items-center px-[20px] py-[10px]">
           <div className="bg-[#E5540A] w-[40px] h-[40px] rounded-[50%] flex items-center justify-center">
             <BsTelephoneFill />
           </div>
           <div>
-            <h3 className="font-bold text-[22px] text-[#FFF]">
+            <h3 className="font-bold text-[18px] text-[#FFF]">
               +233 (0) 50 00 000
             </h3>
           </div>
         </div>
-        <div className="bg-[#1E1E1E] w-[60vw] rounded-[20px] mt-[20px] flex justify-between items-center px-[25px] py-[10px]">
+        <div className="bg-[#1E1E1E] w-[70vw] rounded-[15px] mt-[20px] flex justify-between items-center px-[20px] py-[10px]">
           <div className="bg-[#E5540A] w-[40px] h-[40px] rounded-[50%] flex items-center justify-center">
             <BsTelephoneFill />
           </div>
           <div>
-            <h3 className="font-bold text-[22px] text-[#FFF]">
+            <h3 className="font-bold text-[20px] text-[#FFF]">
               email@gmail.com
             </h3>
           </div>
@@ -35,6 +52,46 @@ export default function ContactMe() {
           <h2 className="text-[#fff] font-bold text-[30px]">
             Send Me an Email
           </h2>
+          <form onSubmit={handleSubmit} className="text-[#fff] flex flex-col">
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={state.name}
+              placeholder="Name"
+              onChange={handleInputChange}
+              required
+              className="mt-[20px] w-[70vw] h-[50px] px-[20px] rounded-[10px] text-[#FFF] bg-[#1E1E1E]"
+            />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={state.email}
+              placeholder="Email"
+              onChange={handleInputChange}
+              required
+              className="mt-[20px] w-[70vw] h-[50px] px-[20px] rounded-[10px] text-[#FFF] bg-[#1E1E1E]"
+            />
+
+            <textarea
+              id="message"
+              name="message"
+              rows="4"
+              value={state.message}
+              placeholder="Message"
+              onChange={handleInputChange}
+              required
+              className="mt-[20px] w-[70vw] px-[20px] rounded-[10px] text-[#FFF] bg-[#1E1E1E]"
+            ></textarea>
+
+            <button
+              type="submit"
+              className="bg-[#E5540A] mt-[20px] w-[70vw] px-[20px] h-[40px] rounded-[10px] "
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
       <div>Image</div>
